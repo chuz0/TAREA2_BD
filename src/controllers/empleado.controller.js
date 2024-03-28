5ac435f509f5ac5e437a394b48d10eb47f45f511
chuz0 <jesvalverde@estudiantec.cr>
2024-03-28 11:26:49 -0600

    COMMIT #3, se continua con la conexion a BD

--- src/controllers/empleado.controller.js
+++ src/controllers/empleado.controller.js
@@ -1,16 +1,27 @@
-import { getConnection } from "../database/conexion"
+import { getConnection, sql } from "../database/conexion";
 
+//obtener todos los empleados
 export const getEmpleado = async   (req, res) => {
-    const pool = await getConnection();
-    const result = await pool.request().query('SELECT * FROM empleado')
-    res.json(result.recordset)
+    try {
+        const pool = await getConnection();
+        const result = await pool.request().query('SELECT * FROM Empleados')
+        res.json(result.recordset)
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
 }
 
+//crear un empleado
 export const createEmpleado = async (req, res) => {
-    const {Nombre, Identificacion, IdPuesto, FechaContratacion, SaldoVacaciones, EsActivo} = req.body
+    const {Puesto, ValorDocumentoIdentidad, Nombre, FechaContratacion} = req.body
 
-    console.log(Nombre, Identificacion, IdPuesto, FechaContratacion, SaldoVacaciones, EsActivo) 
+    if(Puesto == null || ValorDocumentoIdentidad == null || Nombre == null || FechaContratacion == null){
+        return res.status(400).json({msg: 'Rellene todos los campos'})
+    }
 
+    const pool = await getConnection()
+    await pool.request().input('Puesto', sql.VarChar, Puesto).input('ValorDocumentoIdentidad', sql.Int, ValorDocumentoIdentidad).input('Nombre', sql.VarChar, Nombre).input('FechaContratacion', sql.VarChar, FechaContratacion).query('INSERT INTO EMPLEADOS(Puesto, ValorDocumentoIdentidad, Nombre, FechaContratacion) VALUES (@Puesto, @ValorDocumentoIdentidad, @Nombre, @FechaContratacion)')
     res.json('creating empleado')
 }
 
